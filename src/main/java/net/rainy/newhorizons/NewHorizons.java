e76a0a8227785832a7d4ff2475c24b644230228d
Abdulaleam <167490133+Abdulaleam@users.noreply.github.com>
2026-05-20 00:22:24 +0300

    Idk mc mod

---
+++ src/main/java/net/rainy/newhorizons/NewHorizons.java
@@ -0,0 +1,16 @@
+package net.rainy.newhorizons;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NewHorizons implements ModInitializer {
+	public static final String MOD_ID = "new-horizons";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override 
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/rainy/newhorizons/NewHorizonsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rainy.newhorizons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NewHorizonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
